bed7dd01809e424200ae7653e5d38d60e4fde9fc
Peter Kufahl <pkufahl@gmail.com>
2025-01-21 23:22:12 -0700

    add loggin

--- course-info/src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ course-info/src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -1,21 +1,25 @@
 package com.pluralsight.courseinfo.cli;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CourseRetriever {
+    private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
+
     public static void main(String... args) {
-        System.out.println("CourseRetriever started ...");
+        LOG.info("CourseRetriever started ...");
         if (args.length == 0) {
-            System.out.println("Provide author as first argument.");
+            LOG.warn("Provide author as first argument.");
             return;
         }
         try {
             retrieveCourses(args[0]);
         } catch (Exception e) {
-            System.out.println("Unexpected error");
-            e.printStackTrace();
+            LOG.error("Unexpected error", e);
         }
     }
 
     private static void retrieveCourses(String authorId) {
-        System.out.println("Looking up course for author: " + authorId);
+        LOG.info("Looking up course for author: '{}'", authorId);
     }
 }
