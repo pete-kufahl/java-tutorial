73585e990632aa96fd49eda6f3ea4688d0ce1136
Peter Kufahl <pkufahl@gmail.com>
2025-01-22 23:00:51 -0700

    implementation jdbc

--- course-info/course-info-repository/src/main/java/com/pluralsight/courseinfo/repository/CourseJdbcRepository.java
+++ course-info/course-info-repository/src/main/java/com/pluralsight/courseinfo/repository/CourseJdbcRepository.java
@@ -4,11 +4,27 @@ import com.pluralsight.courseinfo.domain.Course;
 import org.h2.jdbcx.JdbcDataSource;
 
 import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class CourseJdbcRepository implements CourseRepository {
+
     private static final String H2_DATABASE_URL =
             "jdbc:h2:file:%s;AUTO_SERVER=TRUE;INIT=RUNSCRIPT FROM './db_init.sql'";
+
+    private static final String INSERT_COURSE = """
+            MERGE INTO Courses (id, name, length, url)
+             VALUES (?, ?, ?, ?)
+            """;
+
+    private static final String ADD_NOTES = """
+            UPDATE Courses SET notes = ?
+             WHERE id = ?
+            """;
+
     private final DataSource dataSource;
 
     public CourseJdbcRepository(String databaseFile) {
@@ -19,11 +35,37 @@ public class CourseJdbcRepository implements CourseRepository {
 
     @Override
     public void SaveCourse(Course course) {
-
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(INSERT_COURSE);
+            statement.setString(1, course.id());
+            statement.setString(2, course.name());
+            statement.setLong(3, course.length());
+            statement.setString(4, course.url());
+            statement.execute();
+        } catch (SQLException e) {
+            // hide the SQL-ness of the implementation: create our own exception type
+            throw new RepositoryException("failed to save " + course, e);
+        }
     }
 
     @Override
     public List<Course> getAllCourses() {
-        return List.of();
+        try (Connection connection = dataSource.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM COURSES");
+            List<Course> courses = new ArrayList<>();
+            while (resultSet.next()) {
+                Course course = new Course(resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getLong(3),
+                        resultSet.getString(4),
+                        Optional.ofNullable(resultSet.getString(5)));
+                courses.add(course);
+            }
+            return Collections.unmodifiableList(courses);
+        } catch (SQLException e) {
+            // hide the SQL-ness of the implementation: create our own exception type
+            throw new RepositoryException("failed to retrieve courses", e);
+        }
     }
 }
