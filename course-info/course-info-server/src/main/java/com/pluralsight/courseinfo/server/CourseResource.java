24ad83ab868286fd440577eba90eb8df6efda6f7
Peter Kufahl <pkufahl@gmail.com>
2025-01-23 20:36:47 -0700

    display as json

--- course-info/course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseResource.java
+++ course-info/course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseResource.java
@@ -2,12 +2,17 @@ package com.pluralsight.courseinfo.server;
 
 import com.pluralsight.courseinfo.domain.Course;
 import com.pluralsight.courseinfo.repository.CourseRepository;
+import com.pluralsight.courseinfo.repository.RepositoryException;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.stream.Collectors;
+import java.util.Comparator;
+import java.util.List;
 
 @Path("/courses")
 public class CourseResource {
@@ -20,10 +25,16 @@ public class CourseResource {
     }
 
     @GET
-    public String getCourses() {
-        return courseRepository.getAllCourses()
-                .stream()
-                .map(Course::toString)
-                .collect(Collectors.joining(", "));
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Course> getCourses() {
+        try {
+            return courseRepository.getAllCourses()
+                    .stream()
+                    .sorted(Comparator.comparing(Course::id))
+                    .toList();
+        } catch (RepositoryException e) {
+            LOG.error("Could not retrieve courses", e);
+            throw new NotFoundException();  // returns 404
+        }
     }
 }
