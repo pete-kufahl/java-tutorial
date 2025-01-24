6071a5228659f657b8f08d006e97e2d611d40418
Peter Kufahl <pkufahl@gmail.com>
2025-01-23 21:14:02 -0700

    add notes - 2

--- course-info/course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseResource.java
+++ course-info/course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseResource.java
@@ -3,16 +3,13 @@ package com.pluralsight.courseinfo.server;
 import com.pluralsight.courseinfo.domain.Course;
 import com.pluralsight.courseinfo.repository.CourseRepository;
 import com.pluralsight.courseinfo.repository.RepositoryException;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.NotFoundException;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Comparator;
-import java.util.List;
+import java.util.stream.Stream;
 
 @Path("/courses")
 public class CourseResource {
@@ -26,15 +23,25 @@ public class CourseResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Course> getCourses() {
+    public Stream<Course> getCourses() {
         try {
             return courseRepository.getAllCourses()
                     .stream()
-                    .sorted(Comparator.comparing(Course::id))
-                    .toList();
+                    .sorted(Comparator.comparing(Course::id));
         } catch (RepositoryException e) {
             LOG.error("Could not retrieve courses", e);
             throw new NotFoundException();  // returns 404
         }
+    }
+
+    @POST
+    @Path("/{id}/notes")
+    @Consumes(MediaType.TEXT_PLAIN)
+    public void addNotes(@PathParam("id") String id, String notes) {
+        /*
+        test with something like:
+        > curl -X POST -H "Content-Type: text/plain" --data "completed as relative beginner" http://localhost:8080/courses/3514597d-487c-41ec-9a30-d55ba28e3089/notes
+         */
+        courseRepository.addNotes(id, notes);
     }
 }
