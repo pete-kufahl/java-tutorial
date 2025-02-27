69b28f8bcfac392a6dbe4cc0fce0bb965fae5712
Peter Kufahl <pkufahl@gmail.com>
2025-02-26 21:58:31 -0700

    set up project - 2

--- advanced/annotations/src/main/java/com/prk/commandline/DemoCommandLine.java
+++ advanced/annotations/src/main/java/com/prk/commandline/DemoCommandLine.java
@@ -1,10 +1,9 @@
 package com.prk.commandline;
 
+import com.prk.commandline.commands.CommandExecutor;
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class DemoCommandLine {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -27,6 +26,37 @@ public class DemoCommandLine {
 
         // interactive loop
         var sessionState = new SessionState();
+        var scanner = new Scanner(System.in);
+        while (!sessionState.isFinished()) {
+            // display the menu
+            System.out.println();
+            for (Class<?> clazz : commandClasses) {
+                Command annotation = clazz.getAnnotation(Command.class);
+                System.out.printf("%-12s - %s%n", annotation.value(), annotation.description());
+            }
 
+            // display prompt, get user input
+            var userInput = UserInput.readUserInput(scanner);
+
+            // find, execute the command
+            boolean found = false;
+            for (Class<?> clazz : commandClasses) {
+                Command annotation = clazz.getAnnotation(Command.class);
+                if (annotation.value().equals(userInput.command())) {
+                    executeCommand(clazz, sessionState, userInput);
+                    found = true;
+                    break;
+                }
+            }
+            // print error if command wasn't found
+            if (!found) {
+                System.out.println("Command not recognized: " + userInput.command());
+            }
+        }
+    }
+
+    private static void executeCommand(Class<?> commandClass, SessionState sessionState, UserInput userInput) {
+        var instance = (CommandExecutor) ClassUtils.newInstance(commandClass);
+        instance.execute(sessionState, userInput);
     }
 }
---
+++ advanced/annotations/src/main/java/com/prk/commandline/commands/AddToShoppingCart.java
@@ -0,0 +1,48 @@
+package com.prk.commandline.commands;
+
+import com.prk.commandline.Command;
+import com.prk.commandline.SessionState;
+import com.prk.commandline.UserInput;
+import com.prk.commandline.model.OrderLine;
+import com.prk.commandline.model.Product;
+
+import java.math.BigDecimal;
+
+@Command(value = "add", order = 30, description = "add products to shopping cart")
+public class AddToShoppingCart implements CommandExecutor {
+
+    @Override
+    public void execute(SessionState sessionState, UserInput userInput) {
+        if (userInput.arguments().length != 2) {
+            System.out.println("usage: add <quantity> <product id>");
+            return;
+        }
+
+        int quantity;
+        try {
+            quantity = Integer.parseInt(userInput.arguments()[0]);
+        } catch (NumberFormatException nfe) {
+            System.out.println("Not a valid quantity: " + userInput.arguments()[0]);
+            return;
+        }
+
+        long productId;
+        try {
+            productId = Long.parseLong(userInput.arguments()[1]);
+        } catch (NumberFormatException nfe) {
+            System.out.println("Not a valid product Id: " + userInput.arguments()[1]);
+            return;
+        }
+
+        var product = Product.Products_By_Id.get(productId);
+        if (product == null) {
+            System.out.println("unknown product id: " + productId);
+            System.out.println("try 'inventory' to list available products");
+            return;
+        }
+
+        var amount = product.price().multiply(BigDecimal.valueOf(quantity));
+        sessionState.getOrderBuilder().addLine(new OrderLine(product, quantity, amount));
+        System.out.printf("add to your shopping cart: %d x %s - $ %s%n", quantity, product.name(), amount);
+    }
+}
