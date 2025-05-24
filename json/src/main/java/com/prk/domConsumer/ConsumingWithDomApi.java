c377bcd63890464fd8e08589e0f950656fc6b7a6
Peter Kufahl <pkufahl@gmail.com>
2025-05-24 10:37:32 -0700

    use DOM api and valildate date fields with update java time library

---
+++ json/src/main/java/com/prk/domConsumer/ConsumingWithDomApi.java
@@ -0,0 +1,46 @@
+package com.prk.domConsumer;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class ConsumingWithDomApi {
+    private static final File BANK_LOAN_FILE = new File("src/main/resources/bank_loan.json");
+
+    public static void main(String[] args) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(BANK_LOAN_FILE);
+
+        ObjectWriter writer = mapper.writerWithDefaultPrettyPrinter();
+        System.out.println(writer.writeValueAsString(jsonNode));
+
+        validateDates(jsonNode);
+    }
+
+    private static void validateDates(JsonNode jsonNode) {
+        var fields = jsonNode.fields();
+        while (fields.hasNext()) {
+            var field = fields.next();
+            String fieldName = field.getKey();
+            JsonNode childNode = field.getValue();
+            if (childNode.isTextual() && fieldName.endsWith("Date")) {
+                String fieldValue = childNode.asText();
+                try {
+                    LocalDate.parse(fieldValue, DateTimeFormatter.ISO_LOCAL_DATE);
+                    System.out.println("validated date field: " + fieldName);
+                } catch (DateTimeParseException e) {
+                    System.out.println("Invalid value: " + fieldValue);
+                }
+            } else if (childNode.isContainerNode()) {
+                validateDates(childNode); // Recurse into nested JSON objects/arrays
+            }
+        }
+    }
+}
