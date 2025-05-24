99d25a23d8e3427531fe932b8dd390d5e66cd213
Peter Kufahl <pkufahl@gmail.com>
2025-05-24 11:03:34 -0700

    use DOM api within a jetty server and servlet class

--- json/src/main/java/com/prk/common/SimpleJettyService.java
+++ json/src/main/java/com/prk/common/SimpleJettyService.java
@@ -1,18 +1,21 @@
 package com.prk.common;
 
+import jakarta.servlet.Servlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.ee10.servlet.ServletHandler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
 
 import java.io.IOException;
 
 public class SimpleJettyService {
+    public static final int PORT = 8000;
 
     public static void main(String[] args) throws Exception {
         // Create a Jetty server on port 8000
-        Server server = new Server(8000);
+        Server server = new Server(PORT);
 
         // Set up context handler
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
@@ -35,6 +38,23 @@ public class SimpleJettyService {
             resp.setContentType("text/plain");
             resp.setStatus(HttpServletResponse.SC_OK);
             resp.getWriter().println("Hello from Jetty!");
+        }
+    }
+
+    public static void run(final Class<? extends Servlet> servlet) {
+        Server server = new Server(PORT);
+        // Use ServletContextHandler (required in Jetty 12+)
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        context.addServlet(servlet, "/*");
+
+        server.setHandler(context);
+        try {
+            server.dumpStdErr();
+            server.start();
+            server.join();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
